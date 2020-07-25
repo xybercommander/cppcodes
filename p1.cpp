3e4d3d203be58f1db239f0f49572c71c1d3d382a
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-25 18:31:08 +0530

    practicing

--- p1.cpp
+++ p1.cpp
@@ -24,52 +24,11 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
-string printRandomString(int n) 
-{ 
-    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
-                          'h', 'i', 'j', 'k', 'l', 'm', 'n',  
-                          'o', 'p', 'q', 'r', 's', 't', 'u', 
-                          'v', 'w', 'x', 'y', 'z' }; 
-  
-    string res = ""; 
-    for (int i = 0; i < n; i++)  
-        res = res + alphabet[rand() % 26]; 
-      
-    return res; 
-} 
-
 kaj_start{ // DIS DA MAIN FUNC
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
-    while(t--) {
-        int n;
-        cin >> n;
-        std::vector<int> v;
-
-        int maxLen = 0;
-        for (int i = 0; i < n; ++i)
-        {
-            int x;
-            cin >> x;
-            v.push_back(x);
-            maxLen = max(maxLen, x);
-        }
-        v.push_back(0);
-
-        string s = printRandomString(maxLen);
-        string prefix = "";
-        
-        cout << s << endl;
-        for(int i = 0; i <= n; i++) {
-            prefix = s.substr(0, v[i]);
-            cout << prefix << endl;
-        }
-
-
-    }
+    
 
     kaj_shesh;
 }
